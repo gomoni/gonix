9888d96a3b10747f3d5b627900c37ca13aa01db4
Michal Vyskocil <michal.vyskocil@gmail.com>
2022-08-22 16:55:11 +0200

    [cksum]: drop unused variable

--- cksum/cksum.go
+++ cksum/cksum.go
@@ -18,7 +18,6 @@ import (
 	"errors"
 	"fmt"
 	"hash"
-	"hash/crc32"
 	"io"
 	"log"
 	"os"
@@ -322,7 +321,6 @@ var crctab = [256]uint32{0x00000000,
 	0x933eb0bb, 0x97ffad0c, 0xafb010b1, 0xab710d06, 0xa6322bdf,
 	0xa2f33668, 0xbcb4666d, 0xb8757bda, 0xb5365d03, 0xb1f740b4,
 }
-var crcTable crc32.Table = crctab
 
 // https://git.suckless.org/sbase/file/cksum.c.html#l74
 func docrc(ctx context.Context, stdio pipe.Stdio) (string, int, error) {
